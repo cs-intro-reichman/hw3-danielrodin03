5855ea4cb1ce80440c47c8a213b5dee872f98d2a
Daniel Rodin <danielrodin@MacBook-Pro-sl-Daniel.local>
2025-11-12 21:42:54 +0200

    hw done

--- Algebra.java
+++ Algebra.java
@@ -25,16 +25,28 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		for (int i = 0; i < x2; i++) {
-			x1++;
+		if (x2 >= 0) {
+			for (int i = 0; i < x2; i++) {
+				x1++;
+			}
+		} else {
+			for (int i = 0; i < x2; i++) {
+				x1--;
+			}
 		}
 		return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		for (int i = 0; i < x2; i++) {
-			x1--;
+		if (x1 >= 0) {
+			for (int i = 0; i < x2; i++) {
+				x1--;
+			}
+		} else {
+			for (int i = 0; i < x2; i++) {
+				x1++;
+			}
 		}
 		return x1;
 	}
@@ -42,10 +54,14 @@ public class Algebra {
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
 		int sum = 0;
-		for (int i = 0; i < x2; i++) {
-			sum = plus(sum, x1);
-		}
-		return sum;
+		boolean neg = (x1 < 0 && x2 > 0) || (x1 > 0 && x2 < 0);
+		if (x1 < 0) x1 = -x1;
+		if (x2 < 0) x2 = -x2;
+			for (int i = 0; i < x2; i++) {
+				sum = plus(sum, x1);
+			}
+		if (neg) return -sum;
+		else return sum;
 	}
 
 	// Returns x^n (for n >= 0)
@@ -60,16 +76,27 @@ public class Algebra {
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
 		int count = 0;
+		boolean neg = (x1 < 0 && x2 > 0) || (x1 > 0 && x2 < 0);
+		if (x1 < 0) x1 = -x1;
+		if (x2 < 0) x2 = -x2;
 		while (x1 >= x2) { 
 			x1 = minus(x1, x2);
 			count++;
 		}
-		return count;
+		if (neg) return -count;
+		else return count;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		return minus(x1, times(x2, div(x1, x2)));
+		boolean neg = x1 < 0;
+		if (x1 < 0) x1 = -x1;
+		if (x2 < 0) x2 = -x2;
+		while (x1 >= x2) { 
+			x1 = minus(x1, x2);
+		}
+		if (neg) return -x1;
+		else return x1;
 	}	
 
 	// Returns the integer part of sqrt(x) 
